854702b667308264584a1e9ce63aa125b1affc4c
saqib_din <din.97legend@gmail.com>
2026-05-23 14:59:10 +0400

    sd

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,17 @@
-// import { fileURLToPath, URL } from 'node:url'
-
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-// import vueDevTools from 'vite-plugin-vue-devtools'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     vue(),
-//     vueDevTools(),
-//   ],
-//   resolve: {
-//     alias: {
-//       '@': fileURLToPath(new URL('./src', import.meta.url))
-//     },
-//   },
-// })
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+
+    sitemap({
+      hostname: 'https://saqib-din.vercel.app'
+    })
+  ],
+
   server: {
     port: 3000
   }
 })
-
